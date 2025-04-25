a23ed95c2503514747a1a7272222fc931e62ce04
lavirez <alirezaalavirazavi@yahoo.com>
2025-04-25 09:57:47 +0330

    Add OfferLiteHero component and update product types for enhanced product display

--- src/types/product.d.ts
+++ src/types/product.d.ts
@@ -1,17 +1,17 @@
 export type Image = {
     url: string,
-    alt: string,
+    alt?: string,
 }
 
 export type Product = {
     id: string,
     image: Image,
-    name: string,
-    price: string,
+    name?: string,
+    price?: string,
     discountPrice?: string,
     discount?: string,
     url: string,
     slug?: string,
-    startDate: string,
-    expireDate: string,
+    startDate?: string,
+    expireDate?: string,
 }
